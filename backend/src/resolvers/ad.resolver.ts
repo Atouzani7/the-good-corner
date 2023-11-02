e3dd37cdd1fdeff21fb037206c2414f38813aedd
Asma <asma.touzani7@gmail.com>
2023-11-02 10:28:36 +0100

    test

--- backend/src/resolvers/ad.resolver.ts
+++ backend/src/resolvers/ad.resolver.ts
@@ -53,44 +53,44 @@ export class AdResolver {
 }
 
 
-//todo ____________ Sans GraphType
+// //todo ____________ Sans GraphType
  
-export const adData = {
-    Query: {
-        listAds: async () => {
-            const ads = new AdsService().list("");
-            return ads; 
-        },
-        listAdsByCategory:async (_:any, {id} : {id: string} ) => {
-            const category = await new CategoryService().find(+id);
-      if (!category) {
-        throw new Error("La catégorie n'existe pas");
-      }
-      const ads = await new AdsService().listByCategory(+id);
-      return ads;
-        },
-         findAdById: async (_: any, {id}:  {id: string}) => { // Arguments (parent, param dans la query .graphQL, le ctx , ) // {id: string} remplacable par : QueryFindAdByIdArgs, trouvé dans le resolver
-                console.log(id);
-                const ad = await new AdsService().find(+id);
-                if (!ad) {
-                  throw new Error("L'annonce n'existe pas");
-                }
-                return ad;
+// export const adData = {
+//     Query: {
+//         listAds: async () => {
+//             const ads = new AdsService().list("");
+//             return ads; 
+//         },
+//         listAdsByCategory:async (_:any, {id} : {id: string} ) => {
+//             const category = await new CategoryService().find(+id);
+//       if (!category) {
+//         throw new Error("La catégorie n'existe pas");
+//       }
+//       const ads = await new AdsService().listByCategory(+id);
+//       return ads;
+//         },
+//          findAdById: async (_: any, {id}:  {id: string}) => { // Arguments (parent, param dans la query .graphQL, le ctx , ) // {id: string} remplacable par : QueryFindAdByIdArgs, trouvé dans le resolver
+//                 console.log(id);
+//                 const ad = await new AdsService().find(+id);
+//                 if (!ad) {
+//                   throw new Error("L'annonce n'existe pas");
+//                 }
+//                 return ad;
           
               
-        }
-    },
-    Mutation: {
-      createAd: async (_: any, { data }: { data: IAdForm }) => {
-            const newAd = await new AdsService().create(data);
-            return newAd;
-    },
-    deleteAd: async (_: any, { id }: { id: MutationDeleteAdArgs }) => { // MutationDeleteAdArgs -> string
-      return await new AdsService().delete(+id);
-    },
-    updateAd: async(_: any, { data }: {data: IUpdateAdForm} ) => {
-      const {id, ...otherData} = data;
-      const adToUpdate = await new AdsService().update(_, otherData);
-      return adToUpdate;
-    }
-}}
+//         }
+//     },
+//     Mutation: {
+//       createAd: async (_: any, { data }: { data: IAdForm }) => {
+//             const newAd = await new AdsService().create(data);
+//             return newAd;
+//     },
+//     deleteAd: async (_: any, { id }: { id: MutationDeleteAdArgs }) => { // MutationDeleteAdArgs -> string
+//       return await new AdsService().delete(+id);
+//     },
+//     updateAd: async(_: any, { data }: {data: IUpdateAdForm} ) => {
+//       const {id, ...otherData} = data;
+//       const adToUpdate = await new AdsService().update(_, otherData);
+//       return adToUpdate;
+//     }
+// }}
