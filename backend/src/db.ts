00fe56aa47e17016a7e9b976dd8e445fa5f01492
Asma <asma.touzani7@gmail.com>
2023-11-29 10:59:13 +0100

    adding docker OK

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,19 @@
 import { DataSource } from "typeorm";
 
 export default new DataSource({
-  type: "sqlite",
-  database: "the_good_corner.sqlite",
-  entities: ["src/entities/*.ts"],
-  synchronize: true, // permet de mettre à jour la BDD lorsqu'on change une entité
-  // logging: ["query","error"],
+  // type: "sqlite",
+  // database: "the_good_corner.sqlite",
+  // entities: ["src/entities/*.ts"],
+  // synchronize: true, // permet de mettre à jour la BDD lorsqu'on change une entité
+  // // logging: ["query","error"],
+  // logging: true,
+  type: 'postgres',
+  host: 'db',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'thegoodcornerDb',
+  synchronize: true,
   logging: true,
+  entities: ["src/entities/*.ts"]
 });
